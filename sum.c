9628075c9df569fd77e74788e53decb8bba4f2e1
TANDRA-BABU <tandrababu24@gmail.com>
2025-01-09 16:23:29 +0530

    Add files via upload
    A C PROGRAM TO FIND THE SUM OF TWO NUMBERS.

---
+++ sum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() //sum of two numbers 
+{
+	int x,y,z;
+	printf("ENTER TWO NUMBERS TO FIND THEIR SUM: ");
+	scanf("%d",&x);
+	scanf("%d",&y);
+	z=x+y;
+	printf("SUM IS %d\n",z);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
